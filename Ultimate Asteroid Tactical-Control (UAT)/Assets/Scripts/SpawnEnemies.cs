46110abeb91d2ba8ddd5849499cd15e00add077c
swoofz <jordanhall.1995@yahoo.com>
2018-11-19 09:33:47 -0800

    Enemy spawner

--- Ultimate Asteroid Tactical-Control (UAT)/Assets/Scripts/SpawnEnemies.cs
+++ Ultimate Asteroid Tactical-Control (UAT)/Assets/Scripts/SpawnEnemies.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour {
 
-    private bool spawn;
-    public List<GameObject> enemies;
+    private Transform tf;
+    private Vector2 randomPos;
 
+    public List<GameObject> enemies;
+    public List<Transform> spawnLocation;
 
     // Use this for initialization
     void Start () {
-
+        tf = GetComponent<Transform>();
+        foreach (Transform child in transform) {
+            spawnLocation.Add(child);
+        }
     }
 
     // Update is called once per frame
     void Update () {
+        randomPos = Random.insideUnitCircle;
+        randomPos *= 200;
+        Transform player = PlayerController.instance.gameObject.transform;
+        Vector3 newPos = new Vector3(player.position.x + randomPos.x, player.position.y + randomPos.y, 0);
 
+        GameObject spawnEnemy = enemies[Random.Range(0, enemies.Count)];
+        Instantiate(spawnEnemy, newPos, spawnEnemy.transform.rotation);
     }
 }
