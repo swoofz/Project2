7e76fcd22a4a7e706ed6c9a01109ec1deb49098f
swoofz <jordanhall.1995@yahoo.com>
2018-11-19 02:12:13 -0800

    Added player movement and player shoot and destory

---
+++ Ultimate Asteroid Tactical-Control (UAT)/Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform tf;   // Create a variable to store our transform component
+    public float speed;     // Create a variable to change movement speed
+
+    // Use this for initialization
+    void Start () {
+        // Load transform component into variable
+        tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move forward in the direction it is facing
+        tf.position += tf.up * speed;
+        // Destory after 5 sec
+        Destroy(gameObject, 5);
+	}
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        // Check if collides with enemy
+        if (collision.gameObject.tag == "Enemy") {
+            Destroy(collision.gameObject);  // Destory enemy
+            Destroy(gameObject);            // Destory bullet 
+        }
+    }
+}
--- Ultimate Asteroid Tactical-Control (UAT)/Assets/Scripts/PlayerController.cs
+++ Ultimate Asteroid Tactical-Control (UAT)/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    //Declare Variables
+    private Transform tf;             // Create a variable to store our transform component
+    public GameObject bullet;         // Create a variable to store a bullet gameObject
+    public float speed = 0.05f;       // Create a variable to change movement speed
+    public float rotSpeed = 5f;       // Create a variable to change rotation speed
+
 	// Use this for initialization
 	void Start () {
-		
+        // Load transform component into variable
+        tf = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () { // Handles Input and player movement
+        // Move Forward
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            tf.position += tf.up * speed;
+        }
+        // Rotate Right
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            tf.Rotate(0,0,-rotSpeed);
+        }
+        // Rotate Left
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            tf.Rotate(0,0,rotSpeed);
+        }
+        // Shoot bullet
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Shoot();
+        }
+    }
+
+    // Create a function fo the player to be able to shoot
+    void Shoot() {
+        // If have a bull gameObject
+        if (bullet != null) {
+            // Instantiate our object
+            Instantiate(bullet, tf.position + tf.up * 1.5f, tf.rotation);
+        } else {
+            Debug.Log("Need gameObject attached");
+        }
+    }
 }
